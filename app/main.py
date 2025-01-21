388fdf0c8aa3dc101f8aa18c81f72a3e464285cd
U039b <git@0x39b.fr>
2025-01-21 17:35:32 +0100

    Generate and publish the OpenAPI specification and the corresponding documentation

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 from mandolin.analyzers.tika_analyzer import Tika
 from mandolin.analyzers.yara_analyzer import Yara
@@ -12,5 +13,24 @@ app.include_router(Thumbnail.get_router())
 
 
 @app.get('/')
-def read_root():
+def root():
     return {'msg': 'Finely slice your files'}
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Mandolin",
+        version="1.0.0",
+        summary="Micro-service to analyze and convert files",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://pts-project.org/android-chrome-512x512.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
