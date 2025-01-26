30301305c135d6389918de75be53f9b13dc46a95
U039b <git@0x39b.fr>
2025-01-26 17:50:53 +0100

    Improve the CI

--- app/main.py
+++ app/main.py
@@ -5,7 +5,14 @@ from mandolin.analyzers.tika_analyzer import Tika
 from mandolin.analyzers.yara_analyzer import Yara
 from mandolin.converters.thumbnail_converter import Thumbnail
 
-app = FastAPI()
+API_VERSION = "1.0.1"
+
+app = FastAPI(
+    title="Mandolin",
+    description="Micro-service to analyze and convert files",
+    summary="Micro-service to analyze and convert files",
+    version=API_VERSION
+)
 
 app.include_router(Tika.get_router())
 app.include_router(Yara.get_router())
@@ -22,8 +29,17 @@ def custom_openapi():
         return app.openapi_schema
     openapi_schema = get_openapi(
         title="Mandolin",
-        version="1.0.0",
-        summary="Micro-service to analyze and convert files",
+        version=API_VERSION,
+        description="Micro-service to analyze and convert files",
+        license_info={
+            "name": "GNU General Public License v3.0",
+            "identifier": "GPL-3.0-or-later",
+        },
+        contact={
+            "name": "U+039b",
+            "url": "https://pts-project.org",
+            "email": "contact@defensive-lab.agency",
+        },
         routes=app.routes,
     )
     openapi_schema["info"]["x-logo"] = {
