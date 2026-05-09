4ae2ef719b1c395340ecb2851a03da702f378f2b
U039b <git@0x39b.fr>
2026-05-09 17:25:17 +0200

    Prepare release 1.2.0

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from mandolin.analyzers.tika_analyzer import Tika
 from mandolin.analyzers.yara_analyzer import Yara
 from mandolin.converters.thumbnail_converter import Thumbnail
 
-API_VERSION = "1.0.3"
+API_VERSION = "1.2.0"
 
 app = FastAPI(
     title="Mandolin",
